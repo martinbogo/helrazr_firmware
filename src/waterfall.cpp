bb784189aadd4740986a00a1aea75400443abb3f
Martin Bogomolni <martinbogo@gmail.com>
2026-05-20 22:51:13 -0700

    Add CC BY-NC-ND 4.0 Copyright headers to all source files

--- src/waterfall.cpp
+++ src/waterfall.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2026 Martin Bogomolni <martinbogo@gmail.com>
+ *
+ * This code is licensed under the Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License (CC BY-NC-ND 4.0).
+ * To view a copy of this license, visit:
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
 #include "waterfall.h"
 #include "lora.h"
 #include "display.h"
